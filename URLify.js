e7e59ddc218bbbc4a4716f8bd91c9004bc839313
Laura Medina <lcmedina390@gmail.com>
2021-12-15 11:46:36 -0600

    cleaned it up

--- URLify.js
+++ URLify.js
@@ -1,8 +1,12 @@
+// Let's create a function that takes in a blog post title and returns the URL-ified version. To create a URL friendly version of a blog post title, all punctuation must be removed, all letters must be lowercase, and all spaces must be joined by hyphens. You should use a combination of regular expressions and string prototype functions to complete this challenge.
 
 function urlify(title){
-    let noPunc = title.replace(/[^\w\s]/g, "");
-    let noSpaces = noPunc.replace(/ /g, "-");
-   let newTitle = noSpaces.toLowerCase()
+    let newTitle = title.replace(/[^\w\s]/g, "")
+    .replace(/ /g, "-")
+    .toLowerCase()
+//     let noPunc = title.replace(/[^\w\s]/g, "");
+//     let noSpaces = noPunc.replace(/ /g, "-");
+//    let newTitle = noSpaces.toLowerCase()
    return console.log(newTitle)
 }
 
